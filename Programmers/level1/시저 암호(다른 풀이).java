fc767004301222be4700228107f5507528ffe977
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2023-07-09 20:45:55 +0900

    Create 시저 암호(다른 풀이).java

---
+++ Programmers/level1/시저 암호(다른 풀이).java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == ' ') answer += ch;
+            else if(Character.isLowerCase(ch))
+                answer += (char)((ch - 'a' + n) % 26 + 'a');
+            else if(Character.isUpperCase(ch))
+                answer += (char)((ch - 'A' + n) % 26 + 'A');
+        }
+
+        return answer;
+    }
+}
