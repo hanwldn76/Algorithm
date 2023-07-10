d5817706f08c4aa6a618e7f7497464ef3040c8ad
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2023-07-10 22:49:27 +0900

    Create 평균 구하기.java

---
+++ Programmers/level1/평균 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for(int i:arr) {
+            answer += i;
+        }
+
+        answer /= arr.length;
+
+        return answer;
+    }
+}
