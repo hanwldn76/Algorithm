c39f89f052f399e6986f8dcfe61ef60d3dd4dcf6
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-02-05 21:27:28 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -2,11 +2,12 @@ h, m = input().split()
 
 h, m = int(h), int(m)
 
-m -= 45
-if m < 0:
-    h -= 1
-    if h < 0:
+if m < 45:
+    if h == 0:
         h = 23
-    m += 60
+        m += 60
+    else:
+        h -= 1
+        m += 60
 
-print(h, m)
+print(h, m-45)
