4b4d3940b747e838610121570088980a06cb85d7
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-02-12 15:08:45 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,17 +1,8 @@
-lst1 = list(input().lower())
-lst2 = list(set(lst1))
-dic = dict()
+s = input().upper()
+word = list(set(s))
+cnt = [s.count(i) for i in word]
 
-for i in lst2:
-    dic[i] = lst1.count(i)
-
-max_values = max(dic.values())
-cnt = 0
-ch = ''
-
-for k in dic.keys():
-    if max_values == dic.get(k):
-        cnt += 1
-        ch = k
-
-print(ch.upper() if cnt == 1 else '?')
+if cnt.count(max(cnt)) > 1:
+    print('?')
+else:
+    print(word[cnt.index(max(cnt))])
