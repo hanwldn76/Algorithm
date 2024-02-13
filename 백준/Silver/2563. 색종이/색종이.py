fdc8feb2b003d734be37e287dc1bb8f945915382
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-02-13 23:23:14 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,19 @@
+paper = [[0 for i in range(100)] for j in range(100)]
+
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+    for j in range(x, x+10):
+        for k in range(y, y+10):
+            if paper[j][k] == 1:
+                pass
+            else:
+                paper[j][k] = 1
+
+sum_paper = 0
+
+for i in range(100):
+    sum_paper += sum(paper[i])
+
+print(sum_paper)
