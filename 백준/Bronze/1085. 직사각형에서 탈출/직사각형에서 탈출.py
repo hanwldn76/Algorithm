92885f27f2d342438e9226738bd66578f0a4798e
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-02-25 17:07:48 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,9 +1,5 @@
 x, y, w, h = map(int, input().split())
 
-min1 = x if w-x > x else w-x
-min2 = y if h-y > y else h-y
+lst = [(w-x), (h-y), x, y]
 
-if min1 > min2:
-    print(min2)
-else:
-    print(min1)
+print(min(lst))
