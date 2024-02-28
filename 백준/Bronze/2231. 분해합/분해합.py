b843939b8c095391d4d9c10a3d40c20b4a736e6d
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-02-28 23:13:48 +0900

    [Bronze II] Title: 분해합, Time: 552 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,11 +2,12 @@ n = int(input())
 result = 0
 
 for i in range(n//2, n):
-    s = str(i)
-    sum = 0
-    for j in range(len(s)):
-        sum += int(s[j])
-    if i+sum==n:
+    tmp = i
+    j = 0
+    while tmp:
+        j += tmp % 10
+        tmp //= 10
+    if j+i==n:
         result = i
         break
 
