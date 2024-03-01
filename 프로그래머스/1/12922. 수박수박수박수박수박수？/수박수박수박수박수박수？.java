508d1682cb640f94a719f897e81244b62dfd923e
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-01 15:31:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.84 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int num = n/2;
+        for(int i = 0; i<num; i++) answer += "수박";
+        answer += n%2==0?"":"수";
+        return answer;
+    }
+}
