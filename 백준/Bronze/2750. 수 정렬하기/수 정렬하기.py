9164ca32bc8ffbefd70b4bb21985ed0e325ebe5f
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-02 22:45:17 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,9 @@
 n = int(input())
-lst = []
+lst = list()
 
 for i in range(n):
     lst.append(int(input()))
 
-for i in range(n):
-    for j in range(n-1):
-        if lst[j] > lst[j+1]:
-            tmp = lst[j]
-            lst[j] = lst[j+1]
-            lst[j+1] = tmp
-            
+lst.sort()
+
 print(*lst, sep="\n")
