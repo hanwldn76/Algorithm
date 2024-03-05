0be99ff3017c79f97b62ee066cbc869ed2deda6d
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-05 19:48:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1868 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -4,9 +4,7 @@ n = int(sys.stdin.readline())
 lst = list(map(int, sys.stdin.readline().split()))
 
 sorted_lst = sorted(list(set(lst)))
-idx_lst = [i for i in range(len(sorted_lst))]
-
-dic = dict(zip(sorted_lst, idx_lst))
+dic = {x:idx for idx, x in enumerate(sorted_lst)}
 
 for i in lst:
     print(dic[i], end=" ")
