d22626d8d8db7b1926be34b3cad732d8dd0cf5a9
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-09 23:05:27 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 9.93 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String [] arr = s.split(" ");
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i].equals("")) {
+                answer += " ";
+                continue;
+            }
+
+            answer += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1).toLowerCase() + " ";
+
+        }
+
+        if(s.substring(s.length()-1,s.length()).equals(" ")) return answer;
+        
+        return answer.substring(0, answer.length()-1);
+    }
+}
