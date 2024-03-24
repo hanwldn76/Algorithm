30c85cf097778d2a0e8b4258a34853e1cb874f9b
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-24 17:31:51 +0900

    [Silver V] Title: 숫자 카드, Time: 688 ms, Memory: 153620 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst_n = map(int, input().split())
+m = int(input())
+lst_m = map(int, input().split())
+chk = [0]*m
+
+dic = dict(zip(lst_m, chk))
+
+for i in lst_n:
+    if i in dic.keys():
+        dic[i] = 1
+
+print(*dic.values())
