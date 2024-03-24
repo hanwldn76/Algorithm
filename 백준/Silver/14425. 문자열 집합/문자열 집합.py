af6245a3398ac8658268ebc4db45a70dc7a2f41b
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-24 18:02:40 +0900

    [Silver IV] Title: 문자열 집합, Time: 116 ms, Memory: 36376 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,16 +1,17 @@
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-s = set()
+n, m = map(int, input().split())
+dic = {}
 
 for i in range(n):
     tmp = input()
-    s.add(tmp)
+    dic[tmp] = 0
 
 count = 0
 for i in range(m):
     tmp = input()
-    if tmp in s:
+    if tmp in dic:
         count += 1
 
 print(count)
