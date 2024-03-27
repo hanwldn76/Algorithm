7a1cfa62a4d4c2cdbc0925f8dd886c1d55b00217
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-27 22:13:37 +0900

    [Silver IV] Title: 대칭 차집합, Time: 260 ms, Memory: 81112 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -4,6 +4,5 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 a = set(map(int, input().split()))
 b = set(map(int, input().split()))
-result = list(a-b | b-a)
 
-print(len(result))
+print(len(a-b)+len(b-a))
