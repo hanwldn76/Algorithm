d5fc0cf862c2fe95fbe9144430687ed0870c3e0d
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2024-03-28 19:31:22 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b): # 최대 공약수
+    while b > 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+def lcm(a, b): # 최소 공배수
+    return a * b // gcd(a,b)
+
+a, b = map(int, input().split())
+print(lcm(a,b))
