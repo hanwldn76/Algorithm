bd6be863bfe6a276feafb51494c3a793a6941057
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2025-01-27 20:30:47 +0900

    [Silver III] Title: N과 M (5), Time: 724 ms, Memory: 54244 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static int n, m;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        f(0);
+
+        br.close();
+        bw.close();
+    }
+    
+    static private void f(int cnt) throws IOException {
+        if(cnt == m){
+            for(int l : list){
+                bw.write(l + " ");
+            }
+            bw.newLine();
+            bw.flush();
+        }
+        else{
+            for(int i = 0; i<n; i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    list.add(arr[i]);
+                    f(cnt+1);
+                    visited[i] = false;
+                    list.remove(list.size()-1);
+                }
+            }
+        }
+    }
+}
