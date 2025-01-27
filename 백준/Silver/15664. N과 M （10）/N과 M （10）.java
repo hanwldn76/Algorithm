2a7be14db274f040323a612d643cd983b66c2477
hanwldn76 <138774991+hanwldn76@users.noreply.github.com>
2025-01-27 21:25:39 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static boolean[] visited;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        f(0, 0);
+
+        for(String s : set){
+            bw.write(s);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static private void f(int cnt, int idx) throws IOException {
+        if(cnt == m){
+            for(int l : list){
+                sb.append(l).append(" ");
+            }
+            set.add(sb.toString().trim());
+            sb.delete(0, sb.length());
+            return;
+        }
+        else{
+            for(int i = idx; i<n; i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    list.add(arr[i]);
+                    f(cnt+1, i);
+                    visited[i] = false;
+                    list.remove(list.size()-1);
+                }
+            }
+        }
+    }
+}
